091196bf86a88ef5bd59ae7650bc5d4cd78fdcce
Claholla <claholla@gmail.com>
2021-08-28 19:02:44 -0500

    set up server and connection files with sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
+// Connection dependencies
 require('dotenv').config();
-
+const mysql2 = require("mysql2");
 const Sequelize = require('sequelize');
 
+// Sequelize database setup
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+      host: process.env.DB_HOST,
       dialect: 'mysql',
+      port: process.env.PORT,
       dialectOptions: {
         decimalNumbers: true,
       },
